6e55d0bbe9eb8073de9dc460c7ba594dca988016
mryykt <avrpic9@gmail.com>
2024-04-17 19:35:06 +0900

    update: parallelize 12_pi.cpp

--- 02_openmp/12_pi.cpp
+++ 02_openmp/12_pi.cpp
@@ -1,9 +1,10 @@
 #include <cstdio>
 
 int main() {
-  int n = 10;
+  int n = 1000000000;
   double dx = 1. / n;
   double pi = 0;
+#pragma omp parallel for reduction(+:pi)
   for (int i=0; i<n; i++) {
     double x = (i + 0.5) * dx;
     pi += 4.0 / (1.0 + x * x) * dx;
