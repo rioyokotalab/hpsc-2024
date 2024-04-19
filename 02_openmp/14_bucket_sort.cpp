9c58ab4d1359c6bb2b264f18a4e6c32dd4a8cbfd
mryykt <avrpic9@gmail.com>
2024-04-19 19:26:26 +0900

    update: parallelized 14_bucket_sort.cpp

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -18,6 +18,7 @@ int main() {
   std::vector<int> offset(range,0);
   for (int i=1; i<range; i++) 
     offset[i] = offset[i-1] + bucket[i-1];
+#pragma omp parallel for
   for (int i=0; i<range; i++) {
     int j = offset[i];
     for (; bucket[i]>0; bucket[i]--) {
