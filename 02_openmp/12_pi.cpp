25ea92c2b1e6e7f22dcd52287869a7b2017d8ba7
Vincent Théo <95906158+Agentcastor@users.noreply.github.com>
2024-04-21 22:53:45 +0900

    exo1

--- 02_openmp/12_pi.cpp
+++ 02_openmp/12_pi.cpp
@@ -1,9 +1,11 @@
 #include <cstdio>
+#include <omp.h>
 
 int main() {
-  int n = 10;
+  int n = 100000000;
   double dx = 1. / n;
   double pi = 0;
+#pragma omp parallel for reduction(+:pi)
   for (int i=0; i<n; i++) {
     double x = (i + 0.5) * dx;
     pi += 4.0 / (1.0 + x * x) * dx;
