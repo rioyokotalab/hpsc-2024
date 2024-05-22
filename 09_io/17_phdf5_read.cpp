fe5df9be288ddb360631c90db5158151db6fb9a4
mryykt <avrpic9@gmail.com>
2024-05-22 20:46:40 +0900

    update: change patter of hdf5

--- 09_io/17_phdf5_read.cpp
+++ 09_io/17_phdf5_read.cpp
@@ -5,13 +5,13 @@
 #include "hdf5.h"
 using namespace std;
 
-int main (int argc, char** argv) {
-  hsize_t dim[2] = {2, 2};
+int main(int argc, char **argv){
+  hsize_t dim[2] = {4, 4};
   int mpisize, mpirank;
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
   MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
-  assert(mpisize == dim[0]*dim[1]);
+  assert(mpisize == dim[0] && mpisize == dim[1]);
   hid_t plist = H5Pcreate(H5P_FILE_ACCESS);
   H5Pset_fapl_mpio(plist, MPI_COMM_WORLD, MPI_INFO_NULL);
   hid_t file = H5Fopen("data.h5", H5F_ACC_RDONLY, plist);
@@ -21,15 +21,17 @@ int main (int argc, char** argv) {
   hsize_t N[ndim];
   H5Sget_simple_extent_dims(globalspace, N, NULL);
   hsize_t NX = N[0], NY = N[1];
-  hsize_t Nlocal[2] = {NX/dim[0], NY/dim[1]};
-  hsize_t offset[2] = {mpirank / dim[0], mpirank % dim[0]};
-  for(int i=0; i<2; i++) offset[i] *= Nlocal[i];
-  hsize_t count[2] = {1,1};
-  hsize_t stride[2] = {1,1};
+  hsize_t block[2] = {NX / dim[0], NY / dim[1]};
+  hsize_t Nlocal[2] = {2 * NX / dim[0], 2 * NY / dim[1]};
+  hsize_t offset[2] = {mpirank / (dim[0] / 2), mpirank % (dim[0] / 2)};
+  for (int i = 0; i < 2; i++)
+    offset[i] *= block[i];
+  hsize_t count[2] = {2, 2};
+  hsize_t stride[2] = {Nlocal[0], Nlocal[1]};
   hid_t localspace = H5Screate_simple(2, Nlocal, NULL);
-  H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, Nlocal);
+  H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, block);
   H5Pclose(plist);
-  vector<int> buffer(Nlocal[0]*Nlocal[1]);
+  vector<int> buffer(Nlocal[0] * Nlocal[1]);
   plist = H5Pcreate(H5P_DATASET_XFER);
   H5Pset_dxpl_mpio(plist, H5FD_MPIO_COLLECTIVE);
   auto tic = chrono::steady_clock::now();
@@ -42,9 +44,9 @@ int main (int argc, char** argv) {
   H5Pclose(plist);
   double time = chrono::duration<double>(toc - tic).count();
   int sum = 0;
-  for (int i=0; i<Nlocal[0]*Nlocal[1]; i++)
+  for (int i = 0; i < Nlocal[0] * Nlocal[1]; i++)
     sum += buffer[i];
-  printf("sum=%d\n",sum);
-  printf("N=%d: %lf s (%lf GB/s)\n",NX*NY,time,4*NX*NY/time/1e9);
+  printf("sum=%d\n", sum);
+  printf("N=%d: %lf s (%lf GB/s)\n", NX * NY, time, 4 * NX * NY / time / 1e9);
   MPI_Finalize();
 }
