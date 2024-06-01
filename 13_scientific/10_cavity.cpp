74e530907565b36a3be7096dad511ebb33c517be
mryykt <avrpic9@gmail.com>
2024-06-01 14:53:20 +0900

    add: arrays

--- 13_scientific/10_cavity.cpp
+++ 13_scientific/10_cavity.cpp
@@ -1,10 +1,28 @@
+#include <cstdlib>
+#include <cstring>
 
 #define NX 41
 #define NY 41
 #define NT 500
 #define NIT 50
-#define DX (2/(NX-1))
-#define DY (2/(NY-1))
+#define DX (2 / (NX - 1))
+#define DY (2 / (NY - 1))
 #define DT 0.01
 #define RHO 1
 #define NU 0.02
+
+float *array(size_t size)
+{
+  float *array = (float *)malloc(size);
+  memset(array, 0, size);
+  return array;
+}
+
+int main()
+{
+  float *u, *v, *p, *b;
+  u = array(sizeof(float) * 2 * NX * NY);
+  v = array(sizeof(float) * 2 * NX * NY);
+  p = array(sizeof(float) * 2 * NX * NY);
+  b = array(sizeof(float) * NX * NY);
+}
