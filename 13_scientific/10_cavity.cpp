9115d58c6aaa371214176a9f3d80039a0cf7debe
mryykt <avrpic9@gmail.com>
2024-06-01 15:04:05 +0900

    add: main loops

--- 13_scientific/10_cavity.cpp
+++ 13_scientific/10_cavity.cpp
@@ -25,4 +25,39 @@ int main()
   v = array(sizeof(float) * 2 * NX * NY);
   p = array(sizeof(float) * 2 * NX * NY);
   b = array(sizeof(float) * NX * NY);
+  int from=0,to=1-from;
+  for (int n = 0; n < NT; n++)
+  {
+    for (int j = 1; j < NY - 1; j++)
+    {
+      for (int i = 1; i < NX - 1; i++)
+      {
+        //b[j,i]
+      }
+    }
+
+    for (int it = 0; it < NIT; it++)
+    {
+      for (int j = 1; j < NY - 1; j++)
+      {
+        for (int i = 1; i < NX - 1; i++)
+        {
+          //p[j,i]
+        }
+      }
+    }
+
+    for (int j = 1; j < NY - 1; j++)
+    {
+      for (int i = 1; i < NX - 1; i++)
+      {
+        //u[j,i]
+
+        //v[j,i]
+      }
+    }
+
+    from=1-from;
+    to=1-to;
+  }
 }
