cc21164ad2c2a0c26c84355d227f4d0f712e2b4d
mryykt <avrpic9@gmail.com>
2024-06-10 17:33:55 +0900

    change: use cuda

--- 13_scientific/10_cavity.cu
+++ 13_scientific/10_cavity.cu
@@ -1,7 +1,5 @@
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-#include <math.h>
 
 #define NX 41
 #define NY 41
@@ -13,28 +11,115 @@
 #define RHO 1.0
 #define NU 0.02
 
-#define FROM(x, y) (from * NX * NY + (y) * NX + (x))
-#define TO(x, y) (to * NX * NY + (y) * NX + (x))
-#define Ix(x, y) ((y) * NX + (x))
-#define FROM_(x, y) (from_ * NX * NY + (y) * NX + (x))
-#define TO_(x, y) (to_ * NX * NY + (y) * NX + (x))
+#define BS 16
 
-float *array(size_t size)
+#define Ix(x, y) ((y) * NX + (x))
+
+__global__ void init(float *p, float *u, float *v)
 {
-  float *array = (float *)malloc(sizeof(float) * size);
-  memset(array, 0, size);
-  return array;
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  int j = blockIdx.y * blockDim.y + threadIdx.y;
+  p[Ix(i, j)] = 0;
+  p[NX * NY + Ix(i, j)] = 0;
+  u[Ix(i, j)] = 0;
+  u[NX * NY + Ix(i, j)] = 0;
+  v[Ix(i, j)] = 0;
+  v[NX * NY + Ix(i, j)] = 0;
+}
+
+__global__ void step(float *p, float *u, float *v)
+{
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  int j = blockIdx.y * blockDim.y + threadIdx.y;
+  float *pn = p + NY * NX;
+  float *un = u + NY * NX;
+  float *vn = v + NY * NX;
+  if (i >= NX || j >= NY)
+  {
+    return;
+  }
+
+  float b = RHO * (1.0 / DT *
+                       ((u[Ix(i + 1, j)] - u[Ix(i - 1, j)]) / (2 * DX) + (v[Ix(i, j + 1)] - v[Ix(i, j - 1)]) / (2 * DY)) -
+                   powf((u[Ix(i + 1, j)] - u[Ix(i - 1, j)]) / (2 * DX), 2) -
+                   2 * ((u[Ix(i, j + 1)] - u[Ix(i, j - 1)]) / (2 * DY) * (v[Ix(i + 1, j)] - v[Ix(i - 1, j)]) / (2 * DX)) -
+                   powf((v[Ix(i, j + 1)] - v[Ix(i, j - 1)]) / (2 * DY), 2));
+  __syncthreads();
+
+  // p
+  for (int it = 0; it < NIT; it++)
+  {
+    pn[Ix(i, j)] = p[Ix(i, j)];
+
+    __syncthreads();
+
+    p[Ix(i, j)] = (powf(DY, 2) * (pn[Ix(i + 1, j)] + pn[Ix(i - 1, j)]) +
+                   powf(DX, 2) * (pn[Ix(i, j + 1)] + pn[Ix(i, j - 1)]) -
+                   b * powf(DX, 2) * powf(DY, 2)) /
+                  (2 * (powf(DX, 2) + powf(DY, 2)));
+
+    __syncthreads();
+    if (j == 0)
+    {
+      p[Ix(i, 0)] = p[Ix(i, 1)];
+    }
+    else if (j == NY - 1)
+    {
+      p[Ix(i, NY - 1)] = p[Ix(i, NY - 2)];
+    }
+    else if (i == 0)
+    {
+      p[Ix(0, j)] = p[Ix(1, j)];
+    }
+    else if (i == NX - 1)
+    {
+      p[Ix(NX - 1, j)] = p[Ix(NX - 2, j)];
+    }
+  }
+
+  // u,v
+  un[Ix(i, j)] = u[Ix(i, j)];
+  vn[Ix(i, j)] = v[Ix(i, j)];
+  __syncthreads();
+  if (j == 0)
+  {
+    u[Ix(i, 0)] = 0;
+    v[Ix(i, 0)] = 0;
+  }
+  else if (j == NY - 1)
+  {
+    u[Ix(i, NY - 1)] = 1;
+    v[Ix(i, NY - 1)] = 0;
+  }
+  else if (i == 0)
+  {
+    u[Ix(0, j)] = 0;
+    v[Ix(0, j)] = 0;
+  }
+  else if (i == NX - 1)
+  {
+    u[Ix(NX - 1, j)] = 0;
+    v[Ix(NX - 1, j)] = 0;
+  }
+  else
+  {
+    u[Ix(i, j)] = un[Ix(i, j)] - un[Ix(i, j)] * DT / DX * (un[Ix(i, j)] - un[Ix(i - 1, j)]) - vn[Ix(i, j)] * DT / DY * (un[Ix(i, j)] - un[Ix(i, j - 1)]) -
+                  DT / (2 * RHO * DX) * (p[Ix(i + 1, j)] - p[Ix(i - 1, j)]) + NU * DT / powf(DX, 2) * (un[Ix(i + 1, j)] - 2 * un[Ix(i, j)] + un[Ix(i - 1, j)]) +
+                  NU * DT / powf(DY, 2) * (un[Ix(i, j + 1)] - 2 * un[Ix(i, j)] + un[Ix(i, j - 1)]);
+
+    v[Ix(i, j)] = vn[Ix(i, j)] - un[Ix(i, j)] * DT / DX * (vn[Ix(i, j)] - vn[Ix(i - 1, j)]) - vn[Ix(i, j)] * DT / DY * (vn[Ix(i, j)] - vn[Ix(i, j - 1)]) -
+                  DT / (2 * RHO * DY) * (p[Ix(i, j + 1)] - p[Ix(i, j - 1)]) + NU * DT / powf(DX, 2) * (vn[Ix(i + 1, j)] - 2 * vn[Ix(i, j)] + vn[Ix(i - 1, j)]) +
+                  NU * DT / powf(DY, 2) * (vn[Ix(i, j + 1)] - 2 * vn[Ix(i, j)] + vn[Ix(i, j - 1)]);
+  }
 }
 
 int main()
 {
-  float *u, *v, *p, *b;
-  u = array(2 * NX * NY);
-  v = array(2 * NX * NY);
-  p = array(2 * NX * NY);
-  b = array(NX * NY);
-  int from = 0, to = 1;
-  int from_ = 0, to_ = 1;
+  float *u, *v, *p;
+  cudaMallocManaged(&p, 2 * NX * NY * sizeof(float));
+  cudaMallocManaged(&u, 2 * NX * NY * sizeof(float));
+  cudaMallocManaged(&v, 2 * NX * NY * sizeof(float));
+  init<<<dim3((NX + BS - 1) / BS, (NY + BS - 1) / BS, 1), dim3(BS, BS, 1)>>>(p, u, v);
 
   FILE *ufile = fopen("u.dat", "w");
   FILE *vfile = fopen("v.dat", "w");
@@ -42,83 +127,15 @@ int main()
 
   for (int n = 0; n < NT; n++)
   {
-    from = 1 - from;
-    to = 1 - to;
 
-    for (int j = 1; j < NY - 1; j++)
-    {
-      for (int i = 1; i < NX - 1; i++)
-      {
-        // b[j,i]
-        b[Ix(i, j)] = RHO * (1.0 / DT *
-                                 ((u[FROM(i + 1, j)] - u[FROM(i - 1, j)]) / (2 * DX) + (v[FROM(i, j + 1)] - v[FROM(i, j - 1)]) / (2 * DY)) -
-                             powf((u[FROM(i + 1, j)] - u[FROM(i - 1, j)]) / (2 * DX), 2) -
-                             2 * ((u[FROM(i, j + 1)] - u[FROM(i, j - 1)]) / (2 * DY) * (v[FROM(i + 1, j)] - v[FROM(i - 1, j)]) / (2 * DX)) -
-                             powf((v[FROM(i, j + 1)] - v[FROM(i, j - 1)]) / (2 * DY), 2));
-      }
-    }
-    for (int it = 0; it < NIT; it++)
-    {
-      from_ = 1 - from_;
-      to_ = 1 - to_;
-
-      for (int j = 1; j < NY - 1; j++)
-      {
-        for (int i = 1; i < NX - 1; i++)
-        {
-          // p[j,i]
-          p[TO_(i, j)] = (powf(DY, 2) * (p[FROM_(i + 1, j)] + p[FROM_(i - 1, j)]) +
-                          powf(DX, 2) * (p[FROM_(i, j + 1)] + p[FROM_(i, j - 1)]) -
-                          b[Ix(i, j)] * powf(DX, 2) * powf(DY, 2)) /
-                         (2 * (powf(DX, 2) + powf(DY, 2)));
-        }
-      }
-
-      for (int i = 0; i < NY; i++)
-      {
-        p[TO_(0, i)] = p[TO_(1, i)];
-        p[TO_(NX - 1, i)] = p[TO_(NX - 2, i)];
-      }
-      for (int i = 0; i < NX; i++)
-      {
-        p[TO_(i, 0)] = p[TO_(i, 1)];
-        p[TO_(i, NY - 1)] = p[TO_(i, NY - 2)];
-      }
-    }
-
-    for (int j = 1; j < NY - 1; j++)
-    {
-      for (int i = 1; i < NX - 1; i++)
-      {
-        // u[j,i]
-        u[TO(i, j)] = u[FROM(i, j)] - u[FROM(i, j)] * DT / DX * (u[FROM(i, j)] - u[FROM(i - 1, j)]) - v[FROM(i, j)] * DT / DY * (u[FROM(i, j)] - u[FROM(i, j - 1)]) -
-                      DT / (2 * RHO * DX) * (p[TO_(i + 1, j)] - p[TO_(i - 1, j)]) + NU * DT / powf(DX, 2) * (u[FROM(i + 1, j)] - 2 * u[FROM(i, j)] + u[FROM(i - 1, j)]) +
-                      NU * DT / powf(DY, 2) * (u[FROM(i, j + 1)] - 2 * u[FROM(i, j)] + u[FROM(i, j - 1)]);
-
-        // v[j,i]
-        v[TO(i, j)] = v[FROM(i, j)] - u[FROM(i, j)] * DT / DX * (v[FROM(i, j)] - v[FROM(i - 1, j)]) - v[FROM(i, j)] * DT / DY * (v[FROM(i, j)] - v[FROM(i, j - 1)]) -
-                      DT / (2 * RHO * DY) * (p[TO_(i, j + 1)] - p[TO_(i, j - 1)]) + NU * DT / powf(DX, 2) * (v[FROM(i + 1, j)] - 2 * v[FROM(i, j)] + v[FROM(i - 1, j)]) +
-                      NU * DT / powf(DY, 2) * (v[FROM(i, j + 1)] - 2 * v[FROM(i, j)] + v[FROM(i, j - 1)]);
-      }
-    }
-
-    for (int i = 0; i < NX; i++)
-    {
-      u[TO(i, 0)] = 0;
-      u[TO(i, NY - 1)] = 1.0;
-      v[TO(i, 0)] = v[TO(i, NY - 1)] = 0;
-    }
-    for (int i = 0; i < NY; i++)
-    {
-      u[TO(0, i)] = u[TO(NX - 1, i)] = 0;
-      v[TO(0, i)] = v[TO(NX - 1, i)] = 0;
-    }
+    step<<<dim3((NX + BS - 1) / BS, (NY + BS - 1) / BS, 1), dim3(BS, BS, 1)>>>(p, u, v);
+    cudaDeviceSynchronize();
 
     for (int j = 0; j < NY; j++)
     {
       for (int i = 0; i < NX; i++)
       {
-        fprintf(ufile, "%f ", u[TO(i, j)]);
+        fprintf(ufile, "%f ", u[Ix(i, j)]);
       }
     }
     fprintf(ufile, "\n");
@@ -126,7 +143,7 @@ int main()
     {
       for (int i = 0; i < NX; i++)
       {
-        fprintf(vfile, "%f ", v[TO(i, j)]);
+        fprintf(vfile, "%f ", v[Ix(i, j)]);
       }
     }
     fprintf(vfile, "\n");
@@ -134,7 +151,7 @@ int main()
     {
       for (int i = 0; i < NX; i++)
       {
-        fprintf(pfile, "%f ", p[TO_(i, j)]);
+        fprintf(pfile, "%f ", p[Ix(i, j)]);
       }
     }
     fprintf(pfile, "\n");
